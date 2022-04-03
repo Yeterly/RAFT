408c55af993cf333f0c1415c9e6c9ddefa3736b1
Winvoker <bayhan2000@outlook.com>
2022-04-03 15:13:43 +0300

    calculate all participants

--- get_optical_flow.py
+++ get_optical_flow.py
@@ -36,43 +36,43 @@ def demo(args):
     model = model.module
     model.to(DEVICE)
     model.eval()
-    participant = args.ps
+
     with torch.no_grad():
-        # for participant in os.listdir(ps_path):
-        # Create empyt numpy array for vstacking
-        np_array = []
-        for video in os.listdir(ps_path + "/" + participant):
-            folder_path = ps_path + "/" + participant + "/" + video
-            output_path = base_output_path + "/" + participant + "/"
-            print(folder_path)
-            images = glob.glob(os.path.join(folder_path, "*.png")) + glob.glob(
-                os.path.join(folder_path, "*.jpg")
-            )
+        for participant in os.listdir(ps_path):
+            # Create empyt numpy array for vstacking
+            np_array = []
+            for video in os.listdir(ps_path + "/" + participant):
+                folder_path = ps_path + "/" + participant + "/" + video
+                output_path = base_output_path + "/" + participant + "/"
+                print(folder_path)
+                images = glob.glob(os.path.join(folder_path, "*.png")) + glob.glob(
+                    os.path.join(folder_path, "*.jpg")
+                )
 
-            images = sorted(images, key=natsort)
-            for imfile1, imfile2 in zip(images[:-1], images[1:]):
-                image1 = load_image(imfile1)
-                image2 = load_image(imfile2)
+                images = sorted(images, key=natsort)
+                for imfile1, imfile2 in zip(images[:-1], images[1:]):
+                    image1 = load_image(imfile1)
+                    image2 = load_image(imfile2)
 
-                padder = InputPadder(image1.shape)
-                image1, image2 = padder.pad(image1, image2)
+                    padder = InputPadder(image1.shape)
+                    image1, image2 = padder.pad(image1, image2)
 
-                flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-                # vstack np_array with flow_up
-                flow_up = torch.squeeze(flow_up).cpu().numpy()
-                np_array.append(flow_up)
+                    flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+                    # vstack np_array with flow_up
+                    flow_up = torch.squeeze(flow_up).cpu().numpy()
+                    np_array.append(flow_up)
 
-                print(imfile1, imfile2, "is done")
-                # output_file = os.path.join("test", "frame.flo")
-                # print(flow.shape)
-                # writePFM("frame.pfm", flow)
-            empty_array = np.empty(len(np_array), object)
-            empty_array[:] = np_array
-            # if output path not exist
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            # save flow_up to output path
-            np.save(output_path + video + "_flow.npy", empty_array)
+                    print(imfile1, imfile2, "is done")
+                    # output_file = os.path.join("test", "frame.flo")
+                    # print(flow.shape)
+                    # writePFM("frame.pfm", flow)
+                empty_array = np.empty(len(np_array), object)
+                empty_array[:] = np_array
+                # if output path not exist
+                if not os.path.exists(output_path):
+                    os.makedirs(output_path)
+                # save flow_up to output path
+                np.save(output_path + video + "_flow.npy", empty_array)
 
 
 parser = argparse.ArgumentParser()
@@ -86,7 +86,6 @@ parser.add_argument(
     "--alternate_corr",
     action="store_true",
 )
-parser.add_argument("--ps", default="ps1")
 args = parser.parse_args()
 
 demo(args)
